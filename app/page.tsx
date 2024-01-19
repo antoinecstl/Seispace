1f66d887fee3806386c5640f18e19532008ec02a
Antoine <antoine.castel@ipsa.fr>
2024-01-19 19:59:19 +0100

    First Batch of Work

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
+import Wheel from '@/components/Wheel';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="my-8">
+        <h1 className="text-heading3-bold text-light-1">Welcome to Seispace Shuffle</h1>
+      </div>
+      
+
+      <div className="mt-14 p-8 rounded-3xl bg-primary-600">
+      <Wheel/>
+      </div>
     </main>
   );
 }
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -5,18 +5,24 @@ import Link from "next/link";
 function Topbar() {
 
   return (
-    <nav className='topbar'>
+    <nav className='topbar mt-2'>
         <Link href='/' className='flex items-center gap-4'>
             <Image src='/target.svg' alt='logo' width={32} height={32} />
             <p className='text-heading3-bold text-light-1'>Seispace</p>
         </Link>
 
         <div className='flex items-center gap-1'>
-            <div className='block ml-2'>
-                <Link href='https://twitter.com/SeiSpaceHub'>
-                    <Image src='/target.svg' alt='logo' width={32} height={32} />
+            <div className='flex gap-6 ml-2'>
+                <Link href='https://twitter.com/SeiSpaceHub' className="mt-1">
+                    <Image src='/X_logo_2023.svg' alt='logo' width={26} height={26}/>
+                </Link>
+                <Link href='https://twitter.com/SeiSpaceHub' >
+                    <Image src='/discord.svg' alt='logo' width={30} height={30}/>
                 </Link>
             </div>
+            <button className="bg-primary-500 ml-6 py-1 px-2 text-white text-body-medium rounded-xl">
+                <p>Connect</p>
+            </button>
         </div>
     </nav>
   );
