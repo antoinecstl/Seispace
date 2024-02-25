37e011b827700ee17079c62dc3adffd9373606ac
Antoine <antoine.castel@ipsa.fr>
2024-02-25 19:35:09 +0100

    Responsive modifications

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { Player } from '@/lib/schema/playerdata.Schema'; // Assurez-vous que le chemin est correct
+import React, { useState } from 'react'; // Ajouter useState
+import { Player } from '@/lib/schema/playerdata.Schema'; // Vérifiez le chemin
 
 type RightSidebarProps = {
   players: Player[];
   totalPot: number;
-  lastbet:{ address: string; amount: number; color: string } | null;
+  lastbet: { address: string; amount: number; color: string } | null;
 };
 
-// Fonction pour formatter l'adresse du portefeuille
 const formatWalletAddress = (address: string) => {
   if (address.length > 10) {
     return `${address.slice(0, 10)}.......${address.slice(-8)}`;
@@ -17,14 +17,18 @@ const formatWalletAddress = (address: string) => {
 };
 
 const RightSidebar: React.FC<RightSidebarProps> = ({ players, totalPot, lastbet }) => {
+  const [showAllPlayers, setShowAllPlayers] = useState(false);
+
   const sortedPlayers = [...players].sort((a, b) => {
     const percentageOfPotA = totalPot > 0 ? (a.bet_amount / totalPot) * 100 : 0;
     const percentageOfPotB = totalPot > 0 ? (b.bet_amount / totalPot) * 100 : 0;
     return percentageOfPotB - percentageOfPotA;
   });
 
-  const playersList = sortedPlayers.map((player, index) => {
-    const percentageOfPot = totalPot > 0 ? ((player.bet_amount / totalPot) * 100).toFixed(2) : 0;
+  const displayedPlayers = showAllPlayers ? sortedPlayers : sortedPlayers.slice(0, 10);
+
+  const playersList = displayedPlayers.map((player, index) => {
+    const percentageOfPot = totalPot > 0 ? ((player.bet_amount / totalPot) * 100).toFixed(2) : '0';
     return (
       <div key={index} className='flex items-center gap-2'>
         <span className='block w-4 h-4 rounded-full' style={{ backgroundColor: player.color }}></span>
@@ -38,20 +42,23 @@ const RightSidebar: React.FC<RightSidebarProps> = ({ players, totalPot, lastbet 
   return (
     <section className='pt-4 xl:pt-10 max-xl:px-2'>
       <div className='flex flex-col justify-center items-center'>
-      {lastbet && (
-        <><h3 className='text-heading4-medium max-xl:text-body-medium text-light-1'>
-            Last Bet
-          </h3>
-          <p className="text-white mt-2">{lastbet.amount} $SEI by 
-          <span className='hidden sm:inline min-[1280px]:hidden 2xl:inline' style={{ color: lastbet.color }}> {lastbet.address}</span> 
-          <span className='sm:hidden min-[1280px]:inline 2xl:hidden' style={{ color: lastbet.color }}> {formatWalletAddress(lastbet.address)}</span> 
-          </p></>)}
-        <h3 className='mt-4 text-heading4-medium max-xl:text-body-medium text-light-1'>
-          Players
-        </h3>
-        <div className='mt-4 '>
+        {lastbet && (
+          <>
+            <h3 className='text-heading4-medium max-xl:text-body-medium text-light-1'>Last Bet</h3>
+            <p className="text-white mt-2">{lastbet.amount} $SEI by <span style={{ color: lastbet.color }}>{formatWalletAddress(lastbet.address)}</span></p>
+          </>
+        )}
+        <h3 className='mt-4 text-heading4-medium max-xl:text-body-medium text-light-1'>Players</h3>
+        <div className='mt-4 overflow-auto max-h-96'>
           {playersList}
         </div>
+        {sortedPlayers.length > 10 && (
+          <button 
+            className='mt-1 sm:mt-4 text-primary-600  hover:text-primary-500 transition duration-200 ease-in-out py-2 px-4 rounded'
+            onClick={() => setShowAllPlayers(!showAllPlayers)}>
+            {showAllPlayers ? 'Show Less' : 'Show All Players'}
+          </button>
+        )}
       </div>
     </section>
   );
